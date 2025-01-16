0072bc0a259eea5e9e739108b58c18cc252c6a5d
loki00345 <99328513+loki00345@users.noreply.github.com>
2025-01-16 13:26:24 +0200

    Add files via upload

---
+++ cs31_1/hh/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace cs31_1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Введіть посилання на фото:");
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Введіть шлях для збереження :");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Введіть назву файлу :");
+            string fileName = Console.ReadLine();
+
+            string fullPath = Path.Combine(path, fileName);
+
+            try
+            {
+                Console.WriteLine("Завантаження фото...");
+                await DownloadImage(url, fullPath);
+                Console.WriteLine($"Фото успішно збережено за адресою: {fullPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+
+        static async Task DownloadImage(string url, string path)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                byte[] imageBytes = await client.GetByteArrayAsync(url);
+                await File.WriteAllBytesAsync(path, imageBytes);
+            }
+        }
+    }
+}
